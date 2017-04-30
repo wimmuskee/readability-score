95e73a247fd556a7306846b07270b49ac89b6ae9
Wim Muskee <wimmuskee@gmail.com>
2017-04-30 09:30:51 +0200

    make lib test independent of calculator

--- test/test_lib.py
+++ test/test_lib.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
-from readability_score.calculators.fleschkincaid import *
+from readability_score.textanalyzer import TextAnalyzer
 
 class LibTestCase(TestCase):
     def test_bad_locale(self):
         with self.assertRaises(LookupError):
-            fk = FleschKincaid("some text","bad_locale")
+            t = TextAnalyzer("some text","bad_locale")
 
     def test_good_locale(self):
         try:
-            fk = FleschKincaid("some text","nl_NL")
+            t = TextAnalyzer("some text","nl_NL")
         except:
             fk = None
-        self.assertTrue(hasattr(fk,"scores"),"calculator not made with non default but correct locale")
+        self.assertTrue(hasattr(t,"scores"),"text analyzer not made with non default but correct locale")
