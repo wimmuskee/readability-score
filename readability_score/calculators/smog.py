047bea8e96c9253ca4e53ac902d3a56c96fc6681
Wim Muskee <wimmuskee@gmail.com>
2017-04-30 10:18:56 +0200

    extended SMOG with i-trofimtschuk's suggestions

--- readability_score/calculators/smog.py
+++ readability_score/calculators/smog.py
@@ -4,10 +4,16 @@ This is the SMOG readability calculator
 
 This tool can calculate the readability score of a text
 using the Simple Measure Of Gobbledygook.
+For texts with 30 or more sentences, the calculator gets a sample
+of 10 sentences from the beginning, middle and the end of the text.
+
 http://en.wikipedia.org/wiki/SMOG_%28Simple_Measure_Of_Gobbledygook%29
+http://webpages.charter.net/ghal/SMOG_Readability_Formula_G._Harry_McLaughlin_%281969%29.pdf
 
 Wim Muskee, 2012-2017
 wimmuskee@gmail.com
+
+https://github.com/i-trofimtschuk, 2013
 
 License: GPL-2
 """
@@ -23,6 +29,18 @@ class SMOG(TextAnalyzer):
         self.us_grade = 0
         self.setGrade()
         self.min_age = getMinimumAgeFromUsGrade(self.us_grade)
+
+    def setTextScores(self):
+        """
+        Smog custom wrapper for setting all the scores.
+        """
+        self.setSentences()
+        if self.scores["sent_count"] >= 30:
+            sentence_middle = int(self.scores["sent_count"]/2)
+            self.sentences = self.sentences[:10] + self.sentences[sentence_middle -5:5+ sentence_middle] + self.sentences[-10:]
+            self.scores["sent_count"] = len(self.sentences)
+        self.parseSentences()
+        self.setAverages()
 
     def setGrade(self):
         """
